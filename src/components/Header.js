bc9e59d6950705a51723ebadf00046a2e62013b4
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-01-28 03:51:39 +0900

    style: 색상추가, 제목 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,26 @@
 // 성환
+import styled from "styled-components";
+
+const Main = styled.div`
+  @font-face {
+    font-family: "BienchenSAS-Regular";
+    src: url("L-wenfoto/src/font/bienchen-sas/Bienchen a.ttf")
+      format("truetype");
+    font-weight: normal;
+  }
+  font-family: "BienchenSAS-Regular";
+  font-size: 50px;
+  background-color: #ff9800;
+
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 20px 0px;
+  text-align: center;
+`;
+
+function Header() {
+  return <Main>Löwenfoto</Main>;
+}
+
+export default Header;
