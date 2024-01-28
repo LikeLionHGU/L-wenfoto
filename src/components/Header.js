04d725b1ab50e977228b732b54e581d5f218b469
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-01-28 22:22:21 +0900

    style: font 적용 및 media 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,20 @@
 // 성환
 import styled from "styled-components";
+import { PRIMARY } from "../styles/palette";
 
 const Main = styled.div`
-  @font-face {
-    font-family: "BienchenSAS-Regular";
-    src: url("L-wenfoto/src/font/bienchen-sas/Bienchen a.ttf")
-      format("truetype");
-    font-weight: normal;
-  }
-  font-family: "BienchenSAS-Regular";
-  font-size: 50px;
-  background-color: #ff9800;
-
+  font-size: 80px;
+  background-color: ${PRIMARY.main};
   display: flex;
   justify-content: center;
   width: 100%;
-  padding: 20px 0px;
   text-align: center;
+  font-family: Bienchena;
+  padding: 30px;
+
+  @media (max-width: 500px) {
+    font-size: 40px;
+  }
 `;
 
 function Header() {
