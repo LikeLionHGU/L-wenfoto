8f931d5aaadeac2fa98111acf5c2364265415b4b
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-01-28 20:43:05 +0900

    feat: 무한 롤링 기능 구현

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,26 @@
 // 한나
-import styled from "styled-components";
+import { useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import profileImg from "../img/myprofile.jpeg";
 import hannaImg from "../img/introprofile.jpeg";
 import eunjuImg from "../img/박은주.jpg";
 import sunghwanImg from "../img/심성환.jpg";
 import yeraImg from "../img/최예라.jpg";
+import chatteamImg from "../img/chatteam.jpg";
+import cuteppImg from "../img/cutepp.jpeg";
+import frontendImg from "../img/frontend.jpg";
+import likelionImg from "../img/likelion.jpeg";
+import shadowImg from "../img/shadow.jpeg";
 
 const BannerDiv = styled.div`
   width: 100%;
-  height: 30%;
+  height: 35%;
   display: flex;
   flex-direction: row;
 `;
 const Card = styled.div`
-  width: 218px;
-  height: 272px;
+  width: 250px;
+  height: 270px;
   margin-right: 30px;
   border: none;
   border-radius: 50px;
@@ -30,16 +36,36 @@ const Image = styled.img`
   height: 100%;
   object-fit: cover;
 `;
-
 const Wrap = styled.div`
   display: flex;
   overflow: hidden;
 `;
-
-const RollingList = styled.div``;
+const rollingAnimation = keyframes`
+  0% { transform: translateX(0); }
+  50% { transform: translateX(-100%); }
+  50.01% { transform: translateX(100%); }
+  100% { transform: translateX(0); }
+`;
+const rollingleft2 = keyframes`
+  0% { transition: translateX(0); }
+  100% { transform: translateX(-200%); }
+`;
+const RollingList = styled.div`
+  animation: ${rollingAnimation} 60s linear infinite;
+  &.clone {
+    animation: ${rollingleft2} 60s linear infinite;
+  }
+`;
 
 const Ul = styled.ul`
   display: flex;
+  padding: 0px;
+`;
+const Li = styled.li`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
 `;
 
 const dummyImg = [
@@ -47,7 +73,7 @@ const dummyImg = [
     img: { profileImg },
   },
   {
-    img: { hannaImg },
+    img: { cuteppImg },
   },
   {
     img: { eunjuImg },
@@ -56,37 +82,55 @@ const dummyImg = [
     img: { sunghwanImg },
   },
   {
-    img: { yeraImg },
+    img: { likelionImg },
   },
   {
     img: { yeraImg },
   },
   {
-    img: { yeraImg },
+    img: { chatteamImg },
   },
   {
-    img: { yeraImg },
+    img: { hannaImg },
   },
   {
-    img: { profileImg },
+    img: { frontendImg },
   },
   {
-    img: { profileImg },
+    img: { shadowImg },
   },
 ];
 function Banner() {
+  useEffect(() => {
+    let roller = document.querySelector(".rolling-list");
+    roller.id = "roller1";
+
+    let clone = roller.cloneNode(true);
+    clone.id = "roller2";
+    document.querySelector(".wrap").appendChild(clone);
+
+    document.querySelector("#roller1").style.left = "0px";
+    document.querySelector("#roller2").style.left =
+      document.querySelector(".rolling-list ul").offsetWidth + "px";
+
+    roller.classList.add("original");
+    clone.classList.add("clone");
+  }, []);
+
   return (
     <BannerDiv>
-      <Wrap>
-        <RollingList>
+      <Wrap className="wrap">
+        <RollingList className="rolling-list">
           <Ul>
             {dummyImg.map((item, index) => (
-              <Card key={index}>
-                <Image
-                  src={Object.values(item.img)[0]}
-                  alt={`Image ${index + 1}`}
-                />
-              </Card>
+              <Li key={index}>
+                <Card key={index}>
+                  <Image
+                    src={Object.values(item.img)[0]}
+                    alt={`Image ${index + 1}`}
+                  />
+                </Card>
+              </Li>
             ))}
           </Ul>
         </RollingList>
